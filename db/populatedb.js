a252965d333b49673758b1a6ee160511485cce49
Preslav <119291608+Preslav977@users.noreply.github.com>
2024-10-26 12:36:33 +0300

    Initial commit

---
+++ db/populatedb.js
@@ -0,0 +1,17 @@
+const { Client } = require("pg");
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: process.env.DB_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
